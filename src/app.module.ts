52082ec1f73b391e1b4a5a29c2e440ecdd61c980
Natália Fernandes <73299431+taliaNat007@users.noreply.github.com>
2026-03-25 22:00:55 -0400

    Add files via upload

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProdutoModule } from './modules/produto/produto.module';
+@Module({
+  imports: [ProdutoModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,22 @@
+
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'node:path';
+import { AppModule } from './app.module';
+import expressEjsLayouts from 'express-ejs-layouts';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+  );
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('ejs');
+
+  app.use(expressEjsLayouts);
+  app.set('layout', 'layouts/main');
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
